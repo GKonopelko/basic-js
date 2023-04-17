98c654d572ef4854f76ed8dedf0fecc5edbf12d6
Grigori Konopelko <grigori.konopelko@gmail.com>
2023-04-18 01:28:47 +0300

    feat: do last one

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,10 +16,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getMatrixElementsSum(matrix) {
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        console.log(`  item {${i}, ${j}} - ${matrix[i][j]}`);
+      }
+    }
+    let sumResults = [];
+    let sum;
+    
+    for (j = 0; j < matrix[0].length; j++) {
+      sum = matrix[0][j];
+      console.log("1 - " + sum);
+      for (i = 1; i < matrix.length; i++) {
+        console.log("2 - " + matrix[i][j]);
+        if (matrix[i-1][j] !== 0) {
+        sum += matrix[i][j];
+        console.log("3 - " + sum);
+        }
+      }
+      console.log("4 - " + sum);
+      sumResults.push(sum);
+      console.log("5 - " + sumResults);
+      
+    };
+    let total = sumResults.reduce((a, b) => a + b, 0);
+      console.log("6 - " + total);
+      return total;
+      }
+    
+  
+
+console.log(getMatrixElementsSum( [
+  [0, 1, 1, 2],
+  [0, 5, 0, 0],
+  [2, 0, 3, 3]
+ ]))
 
 module.exports = {
   getMatrixElementsSum
